2dfd2b9ca120a58672db16fb1f309df45c5f0f04
Vladimir Degtyarskiy <qopp@mail.ru>
2018-02-19 12:48:52 +0300

    Раздел 5 строки

--- fixture/session.py
+++ fixture/session.py
@@ -1,5 +1,3 @@
-
-
 class SessionHelper:
 
     def __init__(self, app):
@@ -14,7 +12,7 @@ class SessionHelper:
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+        wd.find_element_by_css_selector('input[type="submit"]').click()
 
     def logout(self):
         wd = self.app.wd
@@ -31,8 +29,11 @@ class SessionHelper:
 
     def is_logged_in_as(self, username):
         wd = self.app.wd
-        return wd.find_element_by_xpath("//div/div[1]/form/b").text == "("+username+")"
+        return self.get_logged_user() == username
 
+    def get_logged_user(self):
+        wd = self.app.wd
+        return wd.find_element_by_xpath("//div/div[1]/form/b").text[1:-1]
 
     def ensure_login(self, username, password):
         wd = self.app.wd
