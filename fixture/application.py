c9236cc1dfc35642287aa488b9fdc526eb77af61
Vladimir Degtyarskiy <qopp@mail.ru>
2018-02-22 16:26:39 +0300

    Раздел 5 Задание 16

--- fixture/application.py
+++ fixture/application.py
@@ -1,4 +1,4 @@
-from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 from fixture.sessionusers import SessionHelperUsers
@@ -6,12 +6,20 @@ from fixture.users import UserHelper
 
 class Application:
 
-    def __init__(self):
-        self.wd = WebDriver(capabilities={"marionette": False})
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.sessionusers = SessionHelperUsers(self)
         self.users = UserHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -22,11 +30,11 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
+        wd.get(self.base_url)
 
     def open_webpage(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
+        wd.get(self.base_url)
 
     def destroy(self):
         self.wd.quit()
