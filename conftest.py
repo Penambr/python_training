5aebe364406f46b031d15bbf1f1136d33c7d93cf
Vladimir Degtyarskiy <qopp@mail.ru>
2018-02-22 17:25:52 +0300

    Раздел 5 Задание 17

--- conftest.py
+++ conftest.py
@@ -28,7 +28,6 @@ def stop(request):
     request.addfinalizer(fin)
     return fixture
 
-
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
     parser.addoption("--target", action="store", default="target.json")
